0553b73a4707f2707797e11b2eb2f3241b92288c
deathline94 <dlb4all@gmail.com>
2023-07-16 01:02:42 +0330

    v1.8

--- sing-REALITY-box.sh
+++ sing-REALITY-box.sh
@@ -173,7 +173,7 @@ esac
 package_name="sing-box-${latest_version}-linux-${arch}"
 
 # Download the latest release package (.tar.gz) from GitHub
-curl -sLo "/root/${package_name}.tar.gz" "https://github.com/SagerNet/sing-box/releases/download/${latest_version}/${package_name}.tar.gz"
+curl -sLo "/root/${package_name}.tar.gz" "https://github.com/SagerNet/sing-box/releases/download/v${latest_version}/${package_name}.tar.gz"
 
 # Extract the package and move the binary to /root
 tar -xzf "/root/${package_name}.tar.gz" -C /root
